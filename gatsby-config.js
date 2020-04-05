c7b97728d3bb79a9b2df44baffd99ab67c1480fb
Luke Bennett <hello@lukebennett.com.au>
2020-04-05 14:23:17 +1000

    Clean up config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,49 @@
-const path = require('path');
+const dotenv = require('dotenv');
 
-require('dotenv').config({
+dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Shopify Starter`,
-    description: `Kick off your next, ecommerce experience with this Gatsby starter. This starter ships with credentials to a shopify demo store so you can try it out immediately.`,
-    author: `@alexanderhorl`,
+    title: 'Gatsby Shopify Starter',
+    description:
+      'Kick off your next, eCommerce experience with this Gatsby starter.',
+    author: '@luke_bennett_',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-layout',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-layout`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     {
-      resolve: `gatsby-source-shopify`,
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: 'src/images',
+      },
+    },
+    {
+      resolve: 'gatsby-source-shopify',
       options: {
         // The domain name of your Shopify shop. This is required.
         // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
         // 'gatsby-source-shopify-test-shop.myshopify.com'.
+        // If you are running your shop on a custom domain, you need to use that
+        // as the shop name, without a trailing slash, for example:
+        // shopName: "gatsby-shop.com",
         shopName: process.env.SHOP_NAME,
 
         // An API access token to your Shopify shop. This is required.
@@ -50,29 +54,27 @@ module.exports = {
         // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
         accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
 
+        // Set the API version you want to use. For a list of available API versions,
+        // see: https://help.shopify.com/en/api/storefront-api/reference/queryroot
+        // Defaults to 2019-07
+        apiVersion: '2020-01',
+
         // Set verbose to true to display a verbose output on `npm run develop`
         // or `npm run build`. This prints which nodes are being fetched and how
         // much time was required to fetch and process the data.
         // Defaults to true.
         verbose: true,
+
+        // Number of records to fetch on each request when building the cache
+        // at startup. If your application encounters timeout errors during
+        // startup, try decreasing this number.
+        paginationSize: 250,
+
+        // List of collections you want to fetch.
+        // Possible values are: 'shop' and 'content'.
+        // Defaults to ['shop', 'content'].
+        includeCollections: ['shop'],
       },
     },
-    {
-      resolve: 'gatsby-plugin-root-import',
-      options: {
-        '~': path.join(__dirname, 'src/'),
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-134421805-1',
-        anonymize: true,
-        respectDNT: true,
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 };
