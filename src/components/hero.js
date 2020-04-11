bed1bda25699e55ac5c8e2ffad7007b527f45f5d
Luke Bennett <hello@lukebennett.com.au>
2020-04-11 17:18:50 +1000

    Style heading and text more consistently

--- src/components/hero.js
+++ src/components/hero.js
@@ -22,8 +22,12 @@ const Hero = () => {
       <div className="absolute inset-0 w-full h-full px-4 sm:px-6">
         <div className="flex items-center justify-center w-full h-full max-w-sm mx-auto">
           <div className="flex flex-col px-4 py-8 leading-none text-center text-white sm:px-6 bg-transparent-black-50">
-            <span className="text-4xl font-extrabold uppercase">{title}</span>
-            <span className="mt-2 text-2xl font-medium">{description}</span>
+            <span className="text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10">
+              {title}
+            </span>
+            <span className="max-w-2xl mx-auto mt-3 text-xl leading-7 sm:mt-4">
+              {description}
+            </span>
           </div>
         </div>
       </div>
